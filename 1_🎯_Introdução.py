c4f4a924970d7e3297edb6ff13cdaa4afbfdd998
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2024-12-02 20:42:24 -0300

    Update 1_🎯_Introdução.py

--- 1_🎯_Introdução.py
+++ 1_🎯_Introdução.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import numpy as np
+import plotly.graph_objects as go
 import requests
 import pandas as pd
-import plotly.express as px
-import numpy as np
+from prophet import Prophet
+from prophet.diagnostics import cross_validation, performance_metrics
+from math import sqrt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+#import plotly.express as px
+
+
 
 #Configuração gerais das páginas
 st.set_page_config(page_title='Pós-Tech FIAP | Tech Challenge Fase 4 | Grupo 59', page_icon=":chart_with_upwards_trend:", layout= 'wide')
@@ -19,23 +26,33 @@ else:
 st.sidebar.image("https://impactospositivos.com/wp-content/uploads/2024/03/FIAP-Apoiador.png", caption="Pós-Tech Data Analytics | Tech Challenge Fase 4 | Grupo 59")
 
 # Página
-st.header('Pós-Tech FIAP Data Analytics')
-st.subheader(':blue[Fase 4] | Grupo 59', divider="gray")
+st.header('Análise Preço Petróleo Brent')
+st.markdown(' Pós-Tech FIAP Data Analytics | :blue[Tech-challenge Fase 4] | Grupo 59')
 
-st.caption("O mercado de petróleo e seus derivados traz consigo voluptuosas cifras e com enfoque em sua :blue[_produção_] e :blue[_comercialização_] que o presente trabalho se debruça.")
+#st.caption("O mercado de petróleo e seus derivados traz consigo voluptuosas cifras e com enfoque em sua :blue[_produção_] e :blue[_comercialização_] que o presente trabalho se debruça.")
 
 ## Visualização no Streamlit
-# Abas ('Tabs')
-aba1, aba2 = st.tabs(['Introdução','Objetivos'])
+aba1, aba2, aba3 = st.tabs(['Introdução','Objetivos', 'Grupo - Integrantes'])
 # aba1
+with aba1:
+	coluna1, coluna2 = st.columns(2)
+	with coluna1:
+		st.markdown("Desde o advento da moderna indústria petrolífera no século XIX¹, o petróleo é uma das :blue[commodities] com grandes impactos políticos e econômicos desde então. ")
+		st.markdown("Com o desenvolvimento do mercado internacional do petróleo, convencionou-se a organizá-lo a partir da cotação de 2 de seus tipos, sendo eles:")
+		st.markdown("- Brent; e")
+		st.markdown("- WIT.")
+		st.markdown("Dessa forma, o presente estudo traz insights das relações entre o __problemas geopolíticos__, __impactos econômicos__, o __comportamento do preço do barril do petróleo Brent__ e qual a __previsão de fechamento do preço do petróleo__, a partir do modelo de Machine Learning desenvolvido no presente artigo.")
+		
+		#e o WTI. O West Texas Intermediate (WTI) é a cotação utilizada sobretudo no mercado dos EUA, em especial para as transações envolvendo os estados do Texas, Dakota do Norte e Novo México. Enquanto que o Brent é tipo de petróleo que rege cerca de 60% do mercado internacional dessa commodity.")
+	with coluna2:
+		st.image("https://images.pexels.com/photos/1716008/pexels-photo-1716008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", caption="Mercado de petróleo")
 
 
+with aba2:
+	st.markdown("O objetivo do presente estudo é de analisar a flutuação do preço do petróleo no intervalo temporal de 1987 a 2024 e trazer um modelo de previsão para 90 dias.")
 
-
-
-# The Google Looker Studio embed URL
-#looker_studio_url = "https://lookerstudio.google.com/embed/reporting/2acfe271-c0ea-4bb2-bded-1835235ac236/page/tEnnC"
-#looker_studio_url = "https://lookerstudio.google.com/embed/reporting/d572c779-7cea-49cb-b82c-1b2d74d2c465/page/SaKXE"
-#components.iframe(looker_studio_url, width=800, height=600)
-
-#<iframe width="600" height="450" src="xxx" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+with aba3:
+	st.markdown("Os __integrantes__ e suas respectivas __RM__:")
+	st.markdown("- Cheila Betina Schilling dos Santos: RM 355693")
+	st.markdown("- Felipe David Abdala: RM 355751")
+	st.markdown("- Lucas Coimbra Rizzo: RM354448")
