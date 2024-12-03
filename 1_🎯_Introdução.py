ca1130bacb0a05b476c0d538f7c4403cdf618a59
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2024-12-03 01:26:00 -0300

    Update 1_🎯_Introdução.py

--- 1_🎯_Introdução.py
+++ 1_🎯_Introdução.py
@@ -27,26 +27,24 @@ st.sidebar.image("https://impactospositivos.com/wp-content/uploads/2024/03/FIAP-
 st.header('Análise Preço Petróleo Brent')
 st.markdown(' Pós-Tech FIAP Data Analytics | :blue[Tech-challenge Fase 4] | Grupo 59')
 
-#st.caption("O mercado de petróleo e seus derivados traz consigo voluptuosas cifras e com enfoque em sua :blue[_produção_] e :blue[_comercialização_] que o presente trabalho se debruça.")
-
 ## Visualização no Streamlit
-# Abas ('Tabs')
 aba1, aba2 = st.tabs(['Introdução','Objetivos'])
-# aba1
-# aba1
 with aba1:
 	coluna1, coluna2 = st.columns(2)
 	with coluna1:
-		st.markdown("Desde o advento da moderna indústria petrolífera no século XIX¹, o petróleo é uma das :blue[commodities] com grandes impactos políticos e econômicos desde então. ")
-		st.markdown("Com o desenvolvimento do mercado internacional do petróleo, convencionou-se a organizá-lo a partir da cotação de 2 de seus tipos, sendo eles:")
-		st.markdown("- Brent; e")
-		st.markdown("- WIT.")
-		st.markdown("Dessa forma, o presente estudo traz insights das relações entre o __problemas geopolíticos__, __impactos econômicos__, o __comportamento do preço do barril do petróleo Brent__ e qual a __previsão de fechamento do preço do petróleo__, a partir do modelo de Machine Learning desenvolvido no presente artigo.")
-		
-		#e o WTI. O West Texas Intermediate (WTI) é a cotação utilizada sobretudo no mercado dos EUA, em especial para as transações envolvendo os estados do Texas, Dakota do Norte e Novo México. Enquanto que o Brent é tipo de petróleo que rege cerca de 60% do mercado internacional dessa commodity.")
+		st.write(''' Desde o advento da moderna indústria petrolífera no século XIX¹, o petróleo é uma das :blue[commodities] com grandes impactos políticos e econômicos desde então.''')
+		st.write(''' Com o desenvolvimento do mercado internacional do petróleo, convencionou-se a organizá-lo a partir da cotação de 2 de seus tipos, sendo eles:
+		   - Brent; e
+		   - WIT.''')
+
 	with coluna2:
 		st.image("https://images.pexels.com/photos/1716008/pexels-photo-1716008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", caption="Mercado de petróleo")
 
 
 with aba2:
-	st.markdown("O objetivo do presente estudo é de analisar a flutuação do preço do petróleo no intervalo temporal de 1987 a 2024 e trazer um modelo de previsão para 90 dias.")
+	st.write('''#### O objetivo do presente estudo é de:''')
+	st.write('''- Analisar a flutuação do preço do petróleo no intervalo temporal de 1987 a 2024 e trazer um modelo de previsão para 90 dias.
+- Criar um dashboard interativo parar gerar, no mínimo, 4 insights sobre a variação do preço do petróleo, envolendo cenários geopolíticas, crises econômicas e demanda global por energia.
+- Desenvolver um modelo de Machine Learning que preveja o preço do petróleo Brent diariamente;
+- Criar um plano para fazer o deploy em produção do modelo, com as ferramentas que são necessárias;
+- Disponibilizar via Streamlit o modelo em produção.''')
