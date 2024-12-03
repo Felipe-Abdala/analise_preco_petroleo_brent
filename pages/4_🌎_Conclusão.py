a630f3d74b9a7fa31e342493785099a8090132f3
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2024-12-02 23:07:11 -0300

    Update 4_🌎_Conclusão.py

--- pages/4_🌎_Conclusão.py
+++ pages/4_🌎_Conclusão.py
@@ -5,27 +5,20 @@ import plotly.graph_objects as go
 import requests
 import pandas as pd
 import numpy as np
-from datetime import date, timedelta
 from prophet import Prophet
 from prophet.diagnostics import cross_validation, performance_metrics
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from math import sqrt
 
-# SideBar e Filtros
-st.sidebar.title('Filtros')
-todos_anos = st.sidebar.checkbox('Dados de todo o período', value = True)
-if todos_anos:
-    ano = ''
-else:
-    ano = st.sidebar.slider('Ano', 1987, 2024)
+
+# SideBar
+st.sidebar.title('Grupo')
+st.sidebar.write('''Os :blue[__integrantes__] e suas respectivas :blue[__RM__]:''')
+st.sidebar.write('''- Cheila Betina Schilling dos Santos: RM 355693
+- Felipe David Abdala: RM 355751
+- Lucas Coimbra Rizzo: RM354448''')
+st.sidebar.image("https://impactospositivos.com/wp-content/uploads/2024/03/FIAP-Apoiador.png", caption="Pós-Tech Data Analytics | Tech Challenge Fase 4 | Grupo 59")
 
 # Título Página
 st.title('Conclusão')
-
-st.write('Oi!!')
-
-
-# Abas ('Tabs')
-aba1, aba2 = st.tabs(['Conclusão', 'Análise Macroeconômica'])
-# aba1
 
