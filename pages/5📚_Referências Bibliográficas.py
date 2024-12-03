77baa6f0bdbf740c9f25c17929416eb566d242db
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2024-12-03 00:47:15 -0300

    Update 5📚_Referências Bibliográficas.py

--- pages/5📚_Referências Bibliográficas.py
+++ pages/5📚_Referências Bibliográficas.py
@@ -9,7 +9,6 @@ from prophet.diagnostics import cross_validation, performance_metrics
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from math import sqrt
 
-
 # SideBar
 st.sidebar.title('Grupo')
 st.sidebar.write('''Os :blue[__integrantes__] e suas respectivas :blue[__RM__]:''')
@@ -22,11 +21,21 @@ st.sidebar.image("https://impactospositivos.com/wp-content/uploads/2024/03/FIAP-
 st.title('Referências bibliográficas')
 
 #Lista com bibliografia
-st.write('''- ¹ https://stackoverflow.com/questions/73677916/multiple-page-app-side-bar-icon-and-background-in-streamlit | data de acesso: 24/11/2024
-- ² https://www.ipea.gov.br/desafios/index.php?option=com_content&view=article&id=2083:catid=28&Itemid=23 | data de acesso: 24/11/2024
-- ³ https://guru.com.vc/glossario/brent/ | data de acesso: 24/11/2024
-- https://blog.streamlit.io/introducing-multipage-apps/#tips-and-tricks | data de acesso: 24/11/2024
-- https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/ | data de acesso: 22/11/2024
-- https://docs.github.com/pt/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax | data de acesso: 29/11/2024
-- https://impactospositivos.com/wp-content/uploads/2024/03/FIAP-Apoiador.png | data de acesso: 24/11/2024 
-- https://images.pexels.com/photos/1716008/pexels-photo-1716008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr | data de acesso: 24/11/2024 ''')
+st.info('''__Petróleo: eventos econômicos, históricos e políticos__''')
+st.write('''- IPEA |  Disponível em: https://www.ipea.gov.br/desafios/index.php?option=com_content&view=article&id=2083:catid=28&Itemid=23 | Data de acesso: 24/11/2024
+- Petróleo Brent | Disponível em: https://guru.com.vc/glossario/brent/ | Data de acesso: 24/11/2024''')
+
+st.info('''__Bibliotecas e Ferramentas__''')
+st.write('''- Prophet (Ferramenta de Previsão) | Disponível em: https://facebook.github.io/prophet/ | Data de acesso: 22/11/2024
+- Scikit-learn (Machine Learning e Métricas) | Disponível em: https://scikit-learn.org/ | Data de acesso: 22/11/2024
+- Plotly (Visualizações Interativas) | Disponível em: https://plotly.com/ | Data de acesso: 22/11/2024''')
+
+st.info('''__Fonte de Dados: API da EIA (Energy Information Administration)__''')
+st.write('''- Disponível em: https://www.eia.gov/opendata/ | Data de acesso: 22/11/2024''')
+st.info('''__Conceitos Estatísticos e Métricas: Erro Absoluto Médio (MAE), Erro Percentual Absoluto Médio (MAPE), Raiz do Erro Quadrático Médio (RMSE)__''')
+st.write('''- Disponível em: https://otexts.com/fpp2/ | Data de acesso: 24/11/2024
+- Disponível em: https://blog.streamlit.io/introducing-multipage-apps/#tips-and-tricks | Data de acesso: 24/11/2024
+- Disponível em: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/ | Data de acesso: 22/11/2024
+- Disponível em: https://docs.github.com/pt/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax | Data de acesso: 29/11/2024
+- Disponível em: https://impactospositivos.com/wp-content/uploads/2024/03/FIAP-Apoiador.png | Data de acesso: 24/11/2024 
+- Disponível em: https://images.pexels.com/photos/1716008/pexels-photo-1716008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr | Data de acesso: 24/11/2024 ''')
