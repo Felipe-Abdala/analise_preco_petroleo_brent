0f9e18e38f4a8bfe9474ce1c3d3fec676473d454
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2024-12-03 21:19:48 -0300

    Update 1_🎯_Introdução.py

--- 1_🎯_Introdução.py
+++ 1_🎯_Introdução.py
@@ -1,14 +1,23 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import numpy as np
+import plotly as plt
+import plotly.express as px
 import plotly.graph_objects as go
 import requests
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+import time
+import datetime
+from datetime import date, timedelta
 from prophet import Prophet
 from prophet.diagnostics import cross_validation, performance_metrics
-from math import sqrt
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-#import plotly.express as px
+from math import sqrt
+from streamlit_plotly_events import plotly_events
+from IPython.display import display
+
 
 
 
