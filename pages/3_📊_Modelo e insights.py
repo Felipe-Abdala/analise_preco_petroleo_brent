0da76f09f0aee7ab22715274b811110c105ffdcf
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2024-12-03 21:11:58 -0300

    Update 3_📊_Modelo e insights.py

--- pages/3_📊_Modelo e insights.py
+++ pages/3_📊_Modelo e insights.py
@@ -54,7 +54,7 @@ with aba1:
     #Modelo
     # Configurações iniciais
     #st.status('''### :blue[__O Modelo está em execução__]: tempo estimado de 3 minutos''')
-    st.warning(''' A execução do modelo tem o :blue[__tempo estimado em 5 minutos__]. Aguarde até a mensagem de :green[__"Concluído"__] no final da página. ''')
+    st.warning(''' A execução do modelo tem o :blue[__tempo estimado em 5 minutos__]. Aguarde até a mensagem de :green[__"Concluído"__] aparecer no final da página. ''')
     api_key = "kjZJakW4UuR2fvPQPEoC5c1Ngyvfj96lnYUj9rcJ"
     start_date = "1987-05-25"
     end_date = "2024-11-30"
